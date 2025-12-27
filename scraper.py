09f717faaa34434c628174cfee36ede2addb3f21
vda213 <152674207+vda213@users.noreply.github.com>
2025-12-27 14:22:42 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,19 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--window-size=1920,1200")
-chrome_options.add_argument("--ignore-certificate-errors")
-chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1200")
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
+# QUAN TRỌNG: chỉ rõ binary chromium
+chrome_options.binary_location = "/usr/bin/chromium"
+
+driver = webdriver.Chrome(options=chrome_options)
 
 driver.get("https://www.nytimes.com")
 print(driver.title)
